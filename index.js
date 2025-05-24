af6265409e45840973cf03289ba59968ec5fd826
vikas thapa <vikasthapa115@email.com>
2025-05-24 16:28:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,142 @@
+let floor=document.querySelector(".floor");
+
+let submit_btn=document.querySelector("#submit_btn");
+
+
+submit_btn.addEventListener("click",(e)=>{
+
+    
+    //document.querySelector(".lift_btn")?:e.preventDefault();
+
+    e.preventDefault()
+    
+    let NoOfFloor=document.querySelector("#NoOfFloor");
+
+    let NoOfLift=document.querySelector("#NoOfLift");
+
+    //console.dir(NoOfFloor.value)
+
+    //console.log(NoOfLift.value)
+
+    let N_floor=document.createElement("div")
+    N_floor.className="N_floor"
+    N_floor.innerHTML=`
+                    <div class="lift_btn">
+                        <button class="up_btn">up</button>
+                        <button class="down_btn">down</button>
+                    </div>
+                    <div class="lift_space">
+                        
+                    </div>
+                    <div class="floor_no">
+                        floor 0
+                    </div>`;
+    floor.prepend(N_floor)
+
+for(let i=1;i<NoOfFloor.value;i++){
+    let N_floor=document.createElement("div")
+    N_floor.className="N_floor"
+    N_floor.innerHTML=`
+                    <div class="lift_btn">
+                        <button class="up_btn">up</button>
+                        <button class="down_btn">down</button>
+                    </div>
+                    <div >
+                        
+                    </div>
+                    <div class="floor_no">
+                        floor${i}
+                    </div>`;
+    floor.prepend(N_floor)
+}
+
+
+
+let lift_space=document.querySelector(".lift_space")
+
+// console.log(lift_space)
+
+let lift=document.createElement("div")
+    lift.className="lift"
+    lift.innerHTML=`
+                <div class="left_door">          
+                </div>
+                <div class="right_door">          
+                </div>
+                `;
+
+for(let i=0;i<NoOfLift.value;i++){
+    let lift=document.createElement("div")
+    lift.className=`lift`
+    lift.innerHTML=`
+                <div >          
+                </div>
+                <div >          
+                </div>
+                `;
+                 
+    lift_space.append(lift)
+    //console.dir(lift)
+
+    //console.log(lift.offsetTop)
+    //lift.offsetTop="500px"
+}
+
+const up_btn=document.getElementsByClassName("up_btn")
+const down_btn=document.getElementsByClassName("down_btn")
+
+//console.log(up_btn)
+
+//console.log(down_btn)
+
+let i=0
+
+function movelift(e){
+    const j=i;
+    //console.log("btn location ",e.pageY);
+    let lift=document.getElementsByClassName("lift")
+    //console.log("lift location ",lift[i]?.offsetTop," lift number ",i)
+    let distance=(lift[i]?.offsetTop-e.pageY)+55;
+    //console.log("distance  ",distance)
+
+    i<=NoOfLift.value&&lift[i].setAttribute("style",`transition-duration: 10000ms; translate:0px -${distance}px;`);
+
+    
+
+    setTimeout(()=>{
+        //console.log("j in set ",j)
+        lift[j].innerHTML=`
+                <div class="left_door">          
+                </div>
+                <div class="right_door">          
+                </div>
+                `;
+        setTimeout(()=>{
+            lift[j].innerHTML=`
+                <div >          
+                </div>
+                <div>          
+                </div>
+                `;
+        i=j
+        console.log("lift =",i)
+        },10000)
+    },10000)
+
+    i<NoOfLift.value?i++:i=0;
+
+    console.log("lift ++ ",i)
+
+}
+
+for(let i=0;i<up_btn.length;i++){
+    up_btn[i].addEventListener("click",(e)=>{
+        movelift(e)
+    })
+
+    down_btn[i].addEventListener("click",(e)=>{
+        movelift(e)
+    })
+}
+
+})
